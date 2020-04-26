1081e57648f9c2285d1a7f7c304b30b8e6cb0c9e
Munkhbold <munkhbold.d@nmtec.co>
2020-04-26 16:27:37 -0500

    add comment

--- w1d6/scripts.js
+++ w1d6/scripts.js
@@ -1,7 +1,18 @@
+/**
+ * @param {Integer} param1 
+ * @param {Integer} param2 
+ * @returns {Integer} max of this two params
+ */
 function max(param1, param2){
     return param1 < param2 ? param2 : param1;
 }
 
+/**
+ * @param {Integer} param1 
+ * @param {Integer} param2 
+ * @param {Integer} param3 
+ * @returns {Integer} max of these 3
+ */
 function maxOfThree(param1, param2, param3){
   if ( param2 < param1 && param1 > param3){
     return param1;
@@ -11,23 +22,41 @@ function maxOfThree(param1, param2, param3){
   return param3;
 }
 
+/**
+ * @param {String} str - length is 1
+ * @returns {Boolean} 
+ */
 function isVowel(str){
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   return vowels.indexOf(str) != -1 ? true : false;
 }
 
+/**
+ * @param {Integer list} intList 
+ * @returns {Integer} sum of all elements
+ */
 function sum(intList){
   return intList.reduce(function(accum, elem){
     return elem + accum
   })
 }
 
+/**
+ * 
+ * @param {Integer list} intList
+ * @returns {Integer} product of all elements
+ */
 function multiply(intList){
   return intList.reduce(function(accum, elem){
     return accum * elem;
   })
 }
 
+/**
+ * 
+ * @param {String} str 
+ * @returns {String} with reversed value
+ */
 function reverse(str){
   let rev = "";
   for(let i=str.length-1; i>=0; i--){
@@ -36,6 +65,11 @@ function reverse(str){
   return rev;
 }
 
+/**
+ * 
+ * @param {String list} words
+ * @returns {String}
+ */
 function findLongestWord(words){
   let l = 0;
   for(let i=0; i<words.length; i++){
@@ -46,29 +80,57 @@ function findLongestWord(words){
   return l;
 }
 
+/**
+ * 
+ * @param {String list} words 
+ * @param {Integer} minLen 
+ * @returns {String list} contains element that length greater than minLen
+ */
 function filterLongWords(words, minLen){
   return words.filter(function(elem){
     return elem.length > minLen;
   })
 }
 
+/**
+ * 
+ * @param {Integer list} digits 
+ * @returns {Integer list} new List multiplied by ten
+ */
 function multiplyByTen(digits){
   return digits.map(function(elem){
     return elem * 10;
   })
 }
 
+/**
+ * 
+ * @param {Integer list} digits 
+ * @returns list with threes
+ */
 function filterEqualThree(digits){
   return digits.filter(function(elem){
     return elem === 3;
   })
 }
 
+/**
+ * 
+ * @param {Integer list} elems 
+ * @returns {Integer} is product of all elements
+ */
 function productAllElements(elems){
   return elems.reduce(function(accum, elem){
     return accum * elem;
   })
 }
+
+/**
+ * 
+ * @param {object or primitive type} expected 
+ * @param {*} fn 
+ * @returns {String} value "TEST SUCCEEDED" or "TEST FAILED"
+ */
 function myFunctionTest(expected, fn){
   let res = fn();
   let resType = typeof(res);
