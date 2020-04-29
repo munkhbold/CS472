3660f980d1f254ba0b33661132524fc8148688ff
Munkhbold <munkhbold.d@nmtec.co>
2020-04-28 19:30:27 -0500

    update ascii

--- w2d2/ascii.js
+++ w2d2/ascii.js
@@ -8,15 +8,8 @@
     frame = null,
     temp = null;
 
-    function loadPage(){
-        document.getElementById("start").onclick = start;
-        document.getElementById("stop").onclick = stop;
-        document.getElementById("animation").onchange = animation;
-        document.getElementById("mysize").onchange = sizeChange;
-        document.getElementById("speed").onchange = speed;
-    }
-
-    function start(){
+    /** Handle start action */
+    function clickStart(){
         document.getElementById("start").disabled = true;
         document.getElementById("stop").disabled = false;
         document.getElementById("mytext").disabled = true;
@@ -24,6 +17,22 @@
         run(250);
     };
 
+    /** Handle stop action */
+    function clickStop(){
+        document.getElementById("start").disabled = false;
+        document.getElementById("stop").disabled = true;
+        document.getElementById("mytext").disabled = false;
+        document.getElementById("animation").disabled = false;
+
+        // stop interval
+        clearInterval(timer);
+        timer = null;
+    };
+
+    /** 
+     * Start moving animation
+     * @param {Integer} speed
+     */
     function run(speed){
         // console.log(turbo);
         text = document.getElementById("mytext");
@@ -32,7 +41,7 @@
 
         if (timer !== null) return;
         // console.log("3>>>>> : " + turbo);
-        timer = setInterval(function () {
+        timer = setInterval(()=> {
             // console.log("3>>>>> : " + turbo);
             if(x < temp.length) text.value  = temp[x];
             else x=0;
@@ -40,37 +49,37 @@
         }, speed); 
     };
 
-    function stop(){
-        document.getElementById("start").disabled = false;
-        document.getElementById("stop").disabled = true;
-        document.getElementById("mytext").disabled = false;
-        document.getElementById("animation").disabled = false;
-
-        clearInterval(timer);
-        timer = null;
-    };
-
-    function animation(){
+    /** You'll choose one of exercise, juggler, bike, dive */
+    function selectAnimation(){
         var text = document.getElementById("mytext");
         text.value = ANIMATIONS[document.getElementById("animation").value].split("=====\n")[0];
         x = 0;
     };
 
-    function sizeChange(){
+    /** Change font size */
+    function changeSize(){
         var size = document.getElementById("mysize");
         document.getElementById("mytext").style.fontSize = size.value + "pt";
     };
 
-    function speed(){
+    /** Change action speed */
+    function changeSpeed(){
         if(document.getElementById("speed").checked == true){
             // turbo = 50;
             run(50);
-        }
-        else{
+        }else{
             // turbo = 250;
             run(250);
         }
-        
     };
+
+    /** Assign event handlers right after page load finishes */
+    function loadPage(){
+        document.getElementById("start").onclick = clickStart;
+        document.getElementById("stop").onclick = clickStop;
+        document.getElementById("animation").onchange = selectAnimation;
+        document.getElementById("mysize").onchange = changeSize;
+        document.getElementById("speed").onchange = changeSpeed;
+    }
     window.onload = loadPage;
-})
+})();
